bc9e97f4fbd6754bde731bf96167ca201c3f944f
jieun <kje0740@gmail.com>
2024-01-17 14:39:08 +0900

    1

---
+++ JavaProject/SistJavaProject/src/day0117/FileReadWriter_05.java
@@ -0,0 +1,152 @@
+package day0117;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileReadWriter_05 {
+
+	static final String FILENAME = "C:\\sist0103\\file\\sungjuk.txt";
+
+	FileReader fr = null;
+	BufferedReader br = null;
+	FileWriter fw = null;
+	Scanner s = new Scanner(System.in);
+
+	public int getMenu() {
+		int num = 0;
+		System.out.println("1.추가	2.전체출력		9.종료");
+		num = Integer.parseInt(s.nextLine());
+		return num;
+	}
+
+	// 정보를 입력해서 파일에 저장
+	public void sungjukWrite() {
+		// 이름 , 점수 입력
+		System.out.print("학생명을 입력하세요: ");
+		String name = s.nextLine();
+		System.out.print("점수를 입력하세요: ");
+		int score = Integer.parseInt(s.nextLine());
+
+		// 파일에 저장
+		try {
+			fw = new FileWriter(FILENAME, true);
+
+			// 입력된 내용 파일에 저장
+			fw.write(name + "\n");
+			fw.write(score + "\n");
+
+			System.out.println("**학생 성적 저장 완료**");
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		} finally {
+			try {
+				fw.close();
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	// 파일 읽어서 계산후 출력
+	public void sungjukRead() {
+		System.out.println("\t**학생성적 정보**");
+		System.out.println();
+		System.out.println("번호\t학생명\t점수\t학점");
+		System.out.println("==========================");
+
+		try {
+			fr = new FileReader(FILENAME);
+			br = new BufferedReader(fr);
+
+			int num = 0;
+
+			br.readLine();
+			br.readLine();
+			br.readLine();
+			br.readLine();
+			while (true) {
+				String name = br.readLine();
+
+				if (name == null) {
+					break;
+				}
+				int score = Integer.parseInt(br.readLine()); // 문자열로 변환
+				String grade = getGrade(score);
+				
+				System.out.println(++num+"\t"+name+"\t"+score+"\t"+grade);
+			}
+			System.out.println("**출력 완료**");
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}finally {
+			try {
+				br.close();
+				fr.close();
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	// 점수에 대한 학점을 반환하는 메서드
+	public String getGrade(int score) {
+		String grade;
+
+		if (score >= 90) {
+			grade = "A";
+		} else if (score >= 80) {
+			grade = "B";
+		} else if (score >= 70) {
+			grade = "C";
+		} else if (score >= 60) {
+			grade = "D";
+		} else {
+			grade = "F";
+		}
+
+		return grade;
+	}
+
+	public static void main(String[] args) {
+		FileReadWriter_05 fir = new FileReadWriter_05();
+
+		while (true) {
+			int num = fir.getMenu();
+			switch (num) {
+			case 1:
+				System.out.print("학생정보 입력");
+				fir.sungjukWrite();
+				break;
+				
+			case 2:
+				System.out.println("학생정보 출력");
+				fir.sungjukRead();
+				break;
+
+			case 9:
+				System.out.println("**프로그램을 종료합니다**");
+				System.exit(0);
+
+			default:
+				System.out.println("번호를 다시 확인후 입력해주세요");
+
+			}
+		}
+
+	}
+
+}
