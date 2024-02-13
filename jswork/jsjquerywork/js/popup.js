dfc455fdc7b3a4c08fb28fe75caad4c97764c7d3
jini8293 <kje0740@gmail.com>
2024-02-13 17:29:20 +0900

    1

---
+++ jswork/jsjquerywork/js/popup.js
@@ -0,0 +1,10 @@
+function pop1() {
+    window.open('../image/쇼핑몰사진/1.jpg', 'event', 'width=500,height=500');
+}
+
+function pop2() {
+    newpopup = window.open('../image/logoimg/box.png', 'event', 'width=500,height=500');
+
+    setTimeout('newpopup.close()',3000);
+}
+
