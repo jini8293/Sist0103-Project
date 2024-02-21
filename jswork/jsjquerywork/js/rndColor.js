4c8b8f4281f2e758b3c79ebe1597dbcd79d37e61
jini8293 <kje0740@gmail.com>
2024-02-21 17:15:46 +0900

    1

---
+++ jswork/jsjquerywork/js/rndColor.js
@@ -0,0 +1,12 @@
+function randomColor() {
+    //rgb 색상얻기
+
+    var r = parseInt(Math.random() * 256);
+    var g = parseInt(Math.random() * 256);
+    var b = parseInt(Math.random() * 256);
+
+    var color = "rgb(" + r + "," + g + "," + b + ")";
+
+    return color;
+
+}
